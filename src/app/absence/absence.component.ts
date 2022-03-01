252052caba650e181833ab2a1facff0b1ddf8258
DavidPU97 <david2772@student.uni-lj.si>
2022-03-01 10:07:09 +0100

    minor changes to absence view

--- src/app/absence/absence.component.ts
+++ src/app/absence/absence.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { GeneralService } from '../general.service';
 // @ts-ignore
 import { Subscription } from 'rxjs/dist/types/internal/Subscription';
-import { Absence, AbsenceDefinition } from '../user.model';
+import { Absence, AbsenceDefinition, User } from '../user.model';
 import { MatTableDataSource } from '@angular/material/table';
 
 
@@ -17,19 +17,30 @@ export class AbsenceComponent implements OnInit, OnDestroy {
 
   absenceDefSub!: Subscription
   absenceSub!: Subscription
+  usersSubscription!: Subscription
 
   absenceDefinitions: AbsenceDefinition[] = [];
   absences: Absence[] = [];
+  absencesSelected: Absence[] = [];
+  employees: User[] = [];
+  table: {employee:string, absence: string}[] = []
   dataSource!: MatTableDataSource<any>;
 
   ngOnInit(): void {
     this.absenceDefSub = this.generalService.absenceDefinitionsListener.subscribe((absencedefs:any) => {
       this.absenceDefinitions = absencedefs;
+      this.absenceDefSub.unsubscribe();
     });
 
     this.absenceSub = this.generalService.absenceListener.subscribe((absences:any) => {
       this.absences = absences;
+      this.absenceSub.unsubscribe();
     });
+
+    this.usersSubscription = this.generalService.usersListener.subscribe((employees:User[]) => {
+      this.employees = employees
+      this.usersSubscription.unsubscribe();
+    })
 
     this.absenceDefinitions = this.generalService.getAbsenceDefinitions();
     if(this.absenceDefinitions.length == 0){
@@ -40,11 +51,29 @@ export class AbsenceComponent implements OnInit, OnDestroy {
     if(this.absences.length == 0){
       this.generalService.fetchAbsences();
     }
+
+    this.employees = this.generalService.getUsers()
+    if(this.employees.length == 0){
+      this.generalService.fetchUsers()
+    }
     
   }
 
   dateChange(e: any){
-    console.log(e.value)
+    let selectedDate = e.value.setHours(0, 0, 0, 0)
+    console.log(selectedDate)
+    console.log(new Date(this.absences[56].dateStart).setHours(0, 0, 0, 0))
+    this.absencesSelected = this.absences.filter(absence => {
+      if(absence.dateStart && absence.dateEnd){
+        let startDate = new Date(absence.dateStart).setHours(0, 0, 0, 0)
+        let endDate = new Date(absence.dateEnd).setHours(0, 0, 0, 0)
+        if(selectedDate >= startDate && selectedDate <= endDate){
+          return true;
+        }
+      }
+      return false;
+    })
+    console.log(this.absencesSelected)
   }
 
   initMatTable(){
@@ -56,9 +85,10 @@ export class AbsenceComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    if(!this.absenceDefSub ||!this.absenceSub){
+    if(!this.absenceDefSub ||!this.absenceSub || !this.usersSubscription){
       return
     }
+    this.usersSubscription.unsubscribe();
     this.absenceDefSub.unsubscribe();
     this.absenceSub.unsubscribe();
   }
